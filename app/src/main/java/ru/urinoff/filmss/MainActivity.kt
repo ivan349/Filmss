8e2b2e0c06e17b71900a2afa4c7a8b9cbc0755b6
Ivan Urinoff <urinoff.vanya@yandex.ru>
2022-06-20 09:50:55 +0500

    Lesson-1

--- app/src/main/java/ru/urinoff/filmss/MainActivity.kt
+++ app/src/main/java/ru/urinoff/filmss/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import ru.urinoff.filmss.navigation.SetupNavHost
 import ru.urinoff.filmss.ui.theme.FilmssTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +19,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FilmssTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+              val navController = rememberNavController()
+                SetupNavHost(navController = navController)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FilmssTheme {
-        Greeting("Android")
-    }
-}
